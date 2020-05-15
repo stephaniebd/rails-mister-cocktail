2f6c2c0740f39061844c56eaadd447de8d6387ba
Stephanie BeauzileD <stephanie.beauzile.d@gmail.com>
2020-05-15 12:12:19 -0400

    created base sturture for new, create, dose_params and destroy methode and created the form_for dose.

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,26 @@
 class DosesController < ApplicationController
+  def new
+    @dose = dose.new
+  end
+
+  def create
+    @dose = dose.new(dose_params)
+    @dose.save
+    if @dose.save
+      redirect_to dose_path(@dose)
+    else
+      render :new
+    end
+  end
+
+  def dose_params
+    params.require(:dose).permit(:name)
+  end
+
+  def destroy
+    @dose = dose.find(params[:id])
+    @dose.destroy
+
+    redirect_to dose_path(@dose)
+  end
 end
